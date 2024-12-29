3d35626adb5bc4aae8b0ba4a14ec2e471eef6d0c
01fe23bcs032 <01fe23bcs032@kletech.ac.in>
2024-12-29 23:18:37 +0530

    Update Prims.cpp

--- Codes/Prims.cpp
+++ Codes/Prims.cpp
@@ -23,6 +23,7 @@ void printMST(vector<int> &parent, vector<vector<int>> &graph, int V) {
         cout << parent[i] << " - " << i << "\t" << graph[i][parent[i]] << " \n";
     }
 }
+
 // Prim's Algorithm to construct and print MST for a graph represented using adjacency matrix
 void primMST(vector<vector<int>> &graph, int V) {
     // Array to store constructed MST
@@ -35,4 +36,50 @@ void primMST(vector<vector<int>> &graph, int V) {
     vector<bool> mstSet(V, false);
 
     // Initialize the key value of the first vertex (arbitrary) to 0
+    key[0] = 0;
+    parent[0] = -1; // First node is always the root of MST
 
+    // The MST will have V vertices
+    for (int count = 0; count < V - 1; count++) {
+        // Pick the minimum key vertex not yet included in MST
+        int u = minKey(key, mstSet, V);
+
+        // Add the picked vertex to the MST set
+        mstSet[u] = true;
+
+        // Update key value and parent index of the adjacent vertices of the picked vertex
+        for (int v = 0; v < V; v++) {
+            // graph[u][v] is the weight of the edge u-v
+            // key[v] is the minimum weight edge connecting v
+            // mstSet[v] is false if v is not yet included in MST
+            if (graph[u][v] && !mstSet[v] && graph[u][v] < key[v]) {
+                parent[v] = u;
+                key[v] = graph[u][v];
+            }
+        }
+    }
+
+    // Print the MST
+    printMST(parent, graph, V);
+}
+
+int main() {
+    int V;
+    cout << "Enter the number of vertices: ";
+    cin >> V;
+
+    // Input adjacency matrix for the graph
+    vector<vector<int>> graph(V, vector<int>(V));
+
+    cout << "Enter the adjacency matrix of the graph:\n";
+    for (int i = 0; i < V; i++) {
+        for (int j = 0; j < V; j++) {
+            cin >> graph[i][j];
+        }
+    }
+
+    // Run Prim's algorithm
+    primMST(graph, V);
+
+    return 0;
+}
